c11dd5fec72bafe68652cf4856eb13327b79ea58
noqman <140384051+noqman@users.noreply.github.com>
2024-08-26 16:23:29 +0800

    Added mode value checking block

--- mode.ts
+++ mode.ts
@@ -20,4 +20,25 @@ export function readModeValue(): number {
     return sumobit.i2cRead(REG_ADD_DIP);
 }
 
+/**
+* Check the mode value (0-15) and returns the result (true/false).
+* @param modevalue The current DIP position. eg: 7
+*/
+//% group="Mode"
+//% weight=15
+//% blockGap=40
+//% blockId=sumobit_check_mode_value
+//% block="Mode % modevalue"
+//% modevalue.min=0 modevalue.max=15
+export function checkMode(modevalue: number): boolean {
+let result = false;
+
+    if (readModeValue() === modevalue) {
+    result = true;
+        }
+
+        return result;
+    }
+
+
 }

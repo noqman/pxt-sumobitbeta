b0cb74c999f40c3e7b363bb8ff487c5ee518246b
noqman <140384051+noqman@users.noreply.github.com>
2024-08-26 15:34:49 +0800

    Update pxt.json, main.ts, mode.ts

---
+++ mode.ts
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Functions for sumo:bit mode.
+ *
+ * Company: Cytron Technologies Sdn Bhd
+ * Website: http://www.cytron.io
+ * Email:   support@cytron.io
+ *******************************************************************************/

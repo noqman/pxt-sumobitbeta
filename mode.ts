b2e1e1b3d89a5078da3e5d6021332c98cda83ef3
noqman <140384051+noqman@users.noreply.github.com>
2024-08-27 09:32:44 +0800

    Added on-event block for mode switch

--- mode.ts
+++ mode.ts
@@ -5,8 +5,20 @@
  * Website: http://www.cytron.io
  * Email:   support@cytron.io
  *******************************************************************************/
-
+const MODE_EVENT_SOURCE = 0x01;
 namespace sumobit {
+
+    let bgFunctionCreated = false;
+
+    // Event type.
+    let eventType = 0;
+
+    // Array for mode value.
+    let modevalueArray: number[] = [];
+
+    // Array for old compare result.
+    let oldCompareResult: boolean[] = [];
+
 
 /**
  * Return mode value (0-15).
@@ -40,5 +52,61 @@ let result = false;
         return result;
     }
 
+/**
+* Check current mode value and do something when true.
+* @param modevalue The value to compare with. eg: 512
+* @param handler Code to run when the event is raised.
+*/
+//% weight=13
+//% blockGap=8
+//% blockId=sumobit_mode_event
+//% block="on MODE value %modevalue"
+//% modevalue.min=0 modevalue.max=15
+export function onModeEvent(modevalue: number, handler: Action): void {
+// Use a new event type everytime a new event is create.
+        eventType++;
+
+        // Add the event info to the arrays.
+        modevalueArray.push(modevalue);
+
+        // Create a placeholder for the old compare result.
+        oldCompareResult.push(false);
+
+        // Register the event.
+        control.onEvent(MODE_EVENT_SOURCE, eventType, handler);
+
+        // Create a function in background if haven't done so.
+        // This function will check for pot value and raise the event if the condition is met.
+        if (bgFunctionCreated == false) {
+            control.inBackground(function () {
+
+                while (true) {
+                    // Loop for all the event created.
+                    for (let i = 0; i < eventType; i++) {
+
+                        // Check if the condition is met.
+                        if (checkMode(modevalueArray[i]) == true) {
+                            // Raise the event if the compare result changed from false to true.
+                            if (oldCompareResult[i] == false) {
+                                control.raiseEvent(MODE_EVENT_SOURCE, i + 1);
+                            }
+
+                            // Save old compare result.
+                            oldCompareResult[i] = true;
+                        }
+                        else {
+                            // Save old compare result.
+                            oldCompareResult[i] = false;
+                        }
+                        basic.pause(20)
+                    }
+                }
+
+            });
+
+            bgFunctionCreated = true;
+        }
+
+    }
 
 }

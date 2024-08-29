e8dbc4fd848a10bbdbaacb7e8281c7d568c8edcc
noqman <140384051+noqman@users.noreply.github.com>
2024-08-29 17:32:06 +0800

    Update opponent.ts

--- opponent.ts
+++ opponent.ts
@@ -21,6 +21,23 @@ enum SensorPosition {
     None = 6
 }
 
+enum Sensor{
+    //% block="left"
+    Left2 = 0,
+
+    //% block="front left"
+    Left1 = 1,
+
+    //% block="front center"
+    Center = 2,
+
+    //% block="front right"
+    Right1 = 3,
+
+    //% block="right"
+    Right2 = 4,
+}
+
 let L  = pins.digitalReadPin(DigitalPin.P16);
 let FL = pins.digitalReadPin(DigitalPin.P15);
 let FC = pins.digitalReadPin(DigitalPin.P14);
@@ -37,21 +54,20 @@ namespace sumobit{
     //% blockGap=8
     //% blockId=sumobit_maker_object_read_digital
     //% block="Read %sensor value"
-    export function OppSensorValue(sensor: SensorPosition): number {
+    export function OppSensorValue(sensor: Sensor): number {
 
         switch (sensor) {
-            case SensorPosition.Left2:
+            case Sensor.Left2:
                 return pins.digitalReadPin(DigitalPin.P16);
-            case SensorPosition.Left1:
+            case Sensor.Left1:
                 return pins.digitalReadPin(DigitalPin.P15);
-            case SensorPosition.Center:
+            case Sensor.Center:
                 return pins.digitalReadPin(DigitalPin.P14);
-            case SensorPosition.Right1:
+            case Sensor.Right1:
                 return pins.digitalReadPin(DigitalPin.P13);
-            case SensorPosition.Right2:
+            case Sensor.Right2:
                 return pins.digitalReadPin(DigitalPin.P12);
-                    default:
-            return -1;
+
         }
 
     }

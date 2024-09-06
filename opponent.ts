5f3f1d8de1cee34e5e36e5094cce6efb05a85d1c
noqman <140384051+noqman@users.noreply.github.com>
2024-09-06 09:20:57 +0800

    Added pullup command for opponent sensor pins

--- opponent.ts
+++ opponent.ts
@@ -1,3 +1,6 @@
+
+
+
 enum SensorPosition {
     //% block="left"
     Left2 = 0,
@@ -37,6 +40,12 @@ enum Sensor{
     //% block="right"
     Right2 = 4,
 }
+
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 
 let L  = pins.digitalReadPin(DigitalPin.P16);
 let FL = pins.digitalReadPin(DigitalPin.P15);
